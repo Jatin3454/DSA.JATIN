e931298c844f06459328a46fa8e1a7c75c6cbd24
Jatin Soni <sonijatin116@gmail.com>
2025-04-11 19:03:44 +0530

    Create Leetcode(274).cpp

---
+++ Leetcode(274).cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+    sort(citations.rbegin(),citations.rend());
+    int n = citations.size();
+    int count=0;
+    for(int i=0;i<n;i++){
+        if(citations[i]>i){
+            count++;
+    }}
+    return count;
+    }
+};
